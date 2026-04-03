c0e435db44cd11b9831900b2b63568afd056ae1f
FH-kaneko-Develop <t.kaneko@fukuya-k.co.jp>
2026-04-03 15:57:49 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+// 2024年10月12日をDateクラスで作成（※月は0始まりなので10月=9）
+const targetDate = new Date(2024, 9, 12);
+
+// Dateクラスのメソッドで年・月・日を取得
+const year = targetDate.getFullYear();
+const month = targetDate.getMonth() + 1; // getMonth() は 0-11
+const day = targetDate.getDate();
+
+// 文字列として出力
+console.log(`${year}年${month}月${day}日`);
